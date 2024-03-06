79d8d4e9bd230d495ccfb1acf9c500b7b1cef0ee
Carlos Chavez <157320374+CarlosOr25@users.noreply.github.com>
2024-03-06 12:02:52 -0400

    Add: opcion eliminar datos cars_data.

--- Proyecto_Concesionaria.cpp
+++ Proyecto_Concesionaria.cpp
@@ -40,40 +40,59 @@ int main (){
     while (!opcionvalida);
 
     ofstream archivo2; 
+    string temp_id;
+    ifstream archivo_temp;
+    ofstream archivo_temp_out;
 
     switch (opcion){
         case 1:
-              cout << "Ingrese el ID: ";
-    cin >> id;
+            cout << "Ingrese el ID: ";
+            cin >> id;
 
-    cout << "Ingrese el fabricante: ";
-    cin >> fabricante;
+            cout << "Ingrese el fabricante: ";
+            cin >> fabricante;
 
-    cout << "Ingrese el modelo: ";
-    cin >> modelo;
+            cout << "Ingrese el modelo: ";
+            cin >> modelo;
 
-    cout << "Ingrese el año: ";
-    cin >> año;
+            cout << "Ingrese el año: ";
+            cin >> año;
 
-    cout << "Ingrese a quien se vendio: ";
-    cin >> vendido_a;
+            cout << "Ingrese a quien se vendio: ";
+            cin >> vendido_a;
 
-    cout << "Ingrese de quién se compro: ";
-    cin >> comprado_a;
+            cout << "Ingrese de quién se compro: ";
+            cin >> comprado_a;
 
-    cout << "Ingrese el precio de venta: ";
-    cin >> vendido_por;
+            cout << "Ingrese el precio de venta: ";
+            cin >> vendido_por;
 
-    cout << "Ingrese el precio de compra: ";
-    cin >> comprado_por;
+            cout << "Ingrese el precio de compra: ";
+            cin >> comprado_por;
 
-    archivo2.open("cars_data.csv", ios::app);
-    archivo2 << id << ";" << fabricante << ";" << modelo << ";" << año << ";" << vendido_a << ";" << comprado_a << ";" << vendido_por << ";" << comprado_por << endl;
-    archivo2.close(); 
+            archivo2.open("cars_data.csv", ios::app);
+            archivo2 << id << ";" << fabricante << ";" << modelo << ";" << año << ";" << vendido_a << ";" << comprado_a << ";" << vendido_por << ";" << comprado_por << endl;
+            archivo2.close(); 
             break;
 
         case 2:
-            cout << "Por implementar." << endl; // Mensaje temporal
+            archivo_temp.open("cars_data.csv");
+            archivo_temp_out.open("temp.csv");
+            cout << "ingrese el id del registro que desea eliminar:" << endl;
+            cin >> temp_id; 
+
+            while (getline(archivo_temp, linea)) {
+                size_t pos = linea.find (';');
+                string current_id = linea.substr(0, pos);
+                if (current_id != temp_id){
+                    archivo_temp_out << linea << endl;
+                }
+            }
+            archivo_temp.close();
+            archivo_temp_out.close();
+
+            remove("cars_data.csv");
+            rename("temp.csv", "cars_data.csv");
             break;
 
         case 3:
@@ -84,7 +103,6 @@ int main (){
             break;  
     }
 }
-
 
 
 
