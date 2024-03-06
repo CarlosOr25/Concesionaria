b4fa3246e6aada7bcaff0b429e7fc719ee09052e
Carlos Chavez <157320374+CarlosOr25@users.noreply.github.com>
2024-03-06 11:39:31 -0400

    primer commit

---
+++ Proyecto_Concesionaria.cpp
@@ -0,0 +1,97 @@
+#include <iostream> 
+#include <string>
+#include <fstream> 
+#include <algorithm>
+#include <sstream>
+#include <cstdlib>
+
+using namespace std;    
+
+string linea;
+string texto;
+string dato;
+string id, fabricante, modelo, año, vendido_a, comprado_a, vendido_por, comprado_por;
+
+int main (){
+    
+    ifstream archivo("cars_data.csv");
+
+    while (getline(archivo, linea)){
+        texto= texto + linea+ "\n";
+    }
+    archivo.close();
+
+    bool opcionvalida = false;
+    int opcion;
+
+    do {
+        cout << "Que desea hacer?" << "\n"
+             << "1. Agregar un dato."   << "\n"
+             << "2. Eliminar un dato."  << "\n"  
+             << "3. Modificar un dato." << "\n";
+        cin >> opcion;
+
+        if (opcion >= 1 && opcion <= 3) {
+            opcionvalida = true;
+        } else {
+            cout << "Opcion invalida. Por favor, ingrese un numero valido." << endl;
+        }
+    } 
+    while (!opcionvalida);
+
+    ofstream archivo2; 
+
+    switch (opcion){
+        case 1:
+              cout << "Ingrese el ID: ";
+    cin >> id;
+
+    cout << "Ingrese el fabricante: ";
+    cin >> fabricante;
+
+    cout << "Ingrese el modelo: ";
+    cin >> modelo;
+
+    cout << "Ingrese el año: ";
+    cin >> año;
+
+    cout << "Ingrese a quien se vendio: ";
+    cin >> vendido_a;
+
+    cout << "Ingrese de quién se compro: ";
+    cin >> comprado_a;
+
+    cout << "Ingrese el precio de venta: ";
+    cin >> vendido_por;
+
+    cout << "Ingrese el precio de compra: ";
+    cin >> comprado_por;
+
+    archivo2.open("cars_data.csv", ios::app);
+    archivo2 << id << ";" << fabricante << ";" << modelo << ";" << año << ";" << vendido_a << ";" << comprado_a << ";" << vendido_por << ";" << comprado_por << endl;
+    archivo2.close(); 
+            break;
+
+        case 2:
+            cout << "Por implementar." << endl; // Mensaje temporal
+            break;
+
+        case 3:
+            cout << "Por implementar." << endl; // Mensaje temporal
+            break;
+
+        default:
+            break;  
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
